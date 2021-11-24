b1db869846b46b09333b6ea9116b840d6de88f9a
Bimo Iman Smartadi <bimoimansmartadi@gmail.com>
2021-11-24 08:40:46 +0700

    Added chefDTO, ChefWithMasakanDTO, MasakanDTO, MasakanWithChefDTO

---
+++ RumahMakanPadang/RumahMakanPadang.api/Chef/DTO/ChefDTO.cs
@@ -0,0 +1,20 @@
+﻿using RumahMakanPadang.api.Masakan.DTO;
+
+namespace RumahMakanPadang.api.Chef.DTO
+{
+    public class ChefDTO
+    {
+
+        public string Nama { get; set; }
+        public int Umur { get; set; }
+        public string Spesialisasi { get; set; }
+        public string KTP { get; set; }
+
+    }
+
+    public class ChefWithMasakanDTO : ChefDTO
+    {
+        public MasakanDTO Masakan{ get; set; }
+    }
+    
+}
---
+++ RumahMakanPadang/RumahMakanPadang.api/Masakan/DTO/MasakanDTO.cs
@@ -0,0 +1,17 @@
+﻿using RumahMakanPadang.api.Chef.DTO;
+
+namespace RumahMakanPadang.api.Masakan.DTO
+{
+    public class MasakanDTO
+    {
+        public string Nama { get; set; }
+        public int Harga { get; set; }
+        public string Deskripsi { get; set; }
+
+    }
+
+    public class MasakanWithChefDTO : MasakanDTO
+    {
+        public ChefDTO Chef { get; set; }
+    }
+}
